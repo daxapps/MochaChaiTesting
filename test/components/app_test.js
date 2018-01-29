f9b5f1fb5c39a28c74981538efbd14c7b7699f5e
Jason Crawford <daxapps777@gmail.com>
2018-01-29 12:09:53 -0700

    Write test to expect child element

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,10 +1,14 @@
 import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  const component = renderComponent(App);
+describe('App', () => {
+  let component;
 
-  it('shows correct text', () => {
-    expect(component).to.contain('React simple starter');
+  beforeEach(() => {
+    component = renderComponent(App);
+  });
+
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
   });
 });
