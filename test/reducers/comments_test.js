56a7248523a212d7c680a45ed525350a767794c1
Jason Crawford <daxapps777@gmail.com>
2018-01-31 12:59:51 -0700

    Complete comments reducer

--- test/reducers/comments_test.js
+++ test/reducers/comments_test.js
@@ -4,11 +4,11 @@ import {  SAVE_COMMENT } from '../../src/actions/types';
 
 describe('Comments Reducer', () => {
   it('handles action with unknown type', () => {
-    expect(commentReducer()).to.eql([]);
+    expect(commentReducer(undefined, {})).to.eql([]);
   });
 
   it('handles action of type of SAVE_COMMENT', () => {
-    const action = { type: SAVE_COMMENT, paylaod: 'new comment' };
-    expect(commentReducer([], action).to.eql(['new comment']));
+    const action = { type: SAVE_COMMENT, payload: 'new comment' };
+    expect(commentReducer([], action)).to.eql(['new comment']);
   });
 });
