8b926678df094e3fba68ff3795f3710a39031fab
Jason Crawford <daxapps777@gmail.com>
2018-02-01 12:27:18 -0700

    Add React TestUtils

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,7 +1,21 @@
 import jsdom from 'jsdom';
 import jquery from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
 
 // Set up testing environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 const $ = jquery(global.window);
+
+// build 'renderComponent' helper that should render a given react class
+function renderComponent(ComponentClass, props, state) {
+  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+
+  return $(ReactDOM.findDOMNode(componentInstance)); // produces HTML
+}
+
+// Build helper for simulating events
+
+// Set up chai-jquery
+
